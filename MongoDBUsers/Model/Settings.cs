731512d224a6c91b5285fb3da4af27f0e603e126
Waleed Odeh <waleed.Odeh@aerdata.com>
2019-04-22 14:42:03 +0300

    The settings class contains properties defined in the appsettings.json file and is used for accessing application settings via objects

--- MongoDBUsers/Model/Settings.cs
+++ MongoDBUsers/Model/Settings.cs
@@ -1,5 +1,7 @@
 ﻿namespace MongoDBUsers.Model
 {
+
+    //The settings class contains properties defined in the appsettings.json file and is used for accessing application settings via objects
     public class Settings
     {
         public string ConnectionString { get; set; }
