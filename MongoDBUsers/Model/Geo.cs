8a21fe864c088ab1d4a0387e3fbc47bcec678453
Waleed Odeh <waleed.Odeh@aerdata.com>
2019-04-22 14:37:19 +0300

    this class represent all data for geo

--- MongoDBUsers/Model/Geo.cs
+++ MongoDBUsers/Model/Geo.cs
@@ -1,12 +1,11 @@
 ﻿namespace MongoDBUsers.Model
 {
+    //this class represent all data for GEO
     public class Geo
     {
         public string lat { get; set; }
 
         public string lng { get; set; }
-
-      
 
     }
 }
